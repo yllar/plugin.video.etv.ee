097a4d9b5535d57e901bfba966ab1eeb57c4ad8d
Üllar Pajus <yllar.pajus@gmail.com>
2014-07-27 04:24:44 +0300

    - Make it work with new page / Uue lehe tugi - support ETV & ETV2 / ETV ja ETV2 tugi - misc / jne

--- addon.py
+++ addon.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#      Copyright (C) 2012 Yllar Pajus
-#      http://loru.mine.nu
+#      Copyright (C) 2014 Yllar Pajus
+#      http://ku.uk.is
 #
 #  This Program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ import os
 import sys
 import urlparse
 import urllib2
-import re
+import json
 
 import xbmc
 import xbmcgui
@@ -44,6 +44,13 @@ __settings__  = xbmcaddon.Addon(id='plugin.video.etv.ee')
 DAYS = int(__settings__.getSetting('days'))
 if DAYS < 1:
   DAYS = 1
+
+class Logger:
+  def write(data):
+    xbmc.log(data)
+  write = staticmethod(write)
+sys.stdout = Logger
+sys.stderr = Logger
 
 class EtvException(Exception):
   pass
@@ -64,9 +71,12 @@ class Etv(object):
 	
   def listChannels(self):
     items = list()
-    item = xbmcgui.ListItem('ETV', iconImage=FANART)
+    item = xbmcgui.ListItem('ETV', iconImage=LOGOETV)
     item.setProperty('Fanart_Image', FANART)
     items.append((PATH + '?channel=%s' % 'etv', item, True))
+    item = xbmcgui.ListItem('ETV2', iconImage=LOGOETV2)
+    item.setProperty('Fanart_Image', FANART2)
+    items.append((PATH + '?channel=%s' % 'etv2', item, True))
     xbmcplugin.addDirectoryItems(HANDLE, items)
     xbmcplugin.endOfDirectory(HANDLE)
 
@@ -91,47 +101,53 @@ class Etv(object):
     xbmcplugin.endOfDirectory(HANDLE)
   
   def listSchedule(self,channel,date):
-    url = 'http://%s.err.ee/arhiiv.php?sort=paev&paev=%s' % (channel,date)
+    year,month,day = date.split("-")
+    url = 'http://%s.err.ee/api/loader/GetTimelineDay/?year=%s&month=%s&day=%s' % (channel,year,month,day)
     buggalo.addExtraData('url', url)
     html = self.downloadUrl(url)
     if not html:
       raise EtvException(ADDON.getLocalizedString(203))
-    
+
+    html = json.loads(html)
     items = list()
-    html = html.replace('<br />', '\n')
-    for s in re.finditer('.*href="arhiiv.php\?id=([^"]+).*"><b>([^<]+).*\(([^)]+)\).*</div>',html):
-      #print s.group(1), s.group(2), s.group(3)
-      title = s.group(2)
-      date = s.group(3)
+    for s in html:
+      if s['Image']:
+        fanart = 'http://static.err.ee/gridfs/%s?width=720' % s['Image']
+      else:
+        fanart = FANART
+      title = s['Header']
+      plot = s['Lead']
       
       infoLabels = {
-	'date' : date,
+	'plot' : plot,
 	'title' : title
       }
 
-      item = xbmcgui.ListItem(title, iconImage = FANART)
+      item = xbmcgui.ListItem(title, iconImage = fanart)
       item.setInfo('video', infoLabels)
       item.setProperty('IsPlayable', 'true')
-      item.setProperty('Fanart_Image', FANART)
-      items.append((PATH + '?vaata=%s' %  s.group(1), item))
-    xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_DATE)
+      item.setProperty('Fanart_Image', fanart)
+      items.append((PATH + '?vaata=%s' %  s['Id'], item))
+    xbmc.executebuiltin("Container.SetViewMode(500)")
+    xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_UNSORTED)
     xbmcplugin.addDirectoryItems(HANDLE, items)
     xbmcplugin.endOfDirectory(HANDLE)     
-    #<a href="arhiiv.php?id=136729"><b>Terevisioon</b> (11.03.2013 06:55)</a>
     
   def getMediaLocation(self,key):
-    url = "http://etv.err.ee/arhiiv.php?id=%s" % key
+    url = "http://etv.err.ee/api/loader/GetTimeLineContent/%s" % key
     buggalo.addExtraData('url', url)
     html = self.downloadUrl(url)
     if html:
-      #loadFlow('flow_player', 'rtmp://media.err.ee/etvsaated','mp4:2013-002695-0010_Kahekone(SUB).mp4',null,0,true);
-      key = re.search('loadFlow.*\'rtmp([^\']+).*mp4:([^\']+)', html, re.DOTALL)
-      if key:
-	return 'rtmp' + key.group(1) + '/' + key.group(2)
+      html = json.loads(html)
+      for url in html['MediaSources']:
+        return 'rtmp%s' % url['Content'].replace('@','_definst_/')
+      raise EtvException(ADDON.getLocalizedString(202))
     else:
       raise EtvException(ADDON.getLocalizedString(202))
     
   def playStream(self,vaata):
+    if vaata == '00000000-0000-0000-0000-000000000000':
+      raise EtvException(ADDON.getLocalizedString(202))
     saade = EtvAddon.getMediaLocation(vaata)
     buggalo.addExtraData('saade', saade)
     playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
@@ -154,14 +170,17 @@ if __name__ == '__main__':
   PARAMS = urlparse.parse_qs(sys.argv[2][1:])
   
   ICON = os.path.join(ADDON.getAddonInfo('path'), 'icon.png')
-  FANART = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
+  FANART = os.path.join(ADDON.getAddonInfo('path'), 'etv.jpg')
+  FANART2 = os.path.join(ADDON.getAddonInfo('path'), 'etv2.jpg')
+  LOGOETV = os.path.join(ADDON.getAddonInfo('path'), 'etv-logo.png')
+  LOGOETV2 = os.path.join(ADDON.getAddonInfo('path'), 'etv2-logo.png')
   
   CACHE_PATH = xbmc.translatePath(ADDON.getAddonInfo("Profile"))
   if not os.path.exists(CACHE_PATH):
     os.makedirs(CACHE_PATH)
     
 
-  buggalo.SUBMIT_URL = 'http://loru.mine.nu/exception/submit.php'
+  buggalo.SUBMIT_URL = 'http://ku.uk.is/exception/submit.php'
   
   EtvAddon = Etv()
   try:
